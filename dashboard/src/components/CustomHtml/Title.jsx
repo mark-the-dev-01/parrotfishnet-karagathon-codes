e5a5d7ca1a005e773ab07a026f9ae4f22c3db5dc
Mark-the-Dev <mark.the.dev.01@gmail.com>
2020-08-09 01:45:30 -0500

    making maps work

---
+++ dashboard/src/components/CustomHtml/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+
+export default class Title extends Component {
+  render() {
+    return (
+      <header>
+        <h2>Title header</h2>
+        <h4>subheader</h4>
+      </header>
+    );
+  }
+}
---
+++ dashboard/src/components/Maps/MapComponent.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./MapComponent.css";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = { lat: 8.545379, lng: 124.566067 };
+const APIKEY = "AIzaSyDC2fjGO1xZloKGZnKz6P4NrOkOuS0degc";
+
+export default class MapComponent extends Component {
+  render() {
+    return (
+      <LoadScript googleMapsApiKey={APIKEY}>
+        <GoogleMap
+          mapContainerClassName="MapComponent"
+          center={center}
+          zoom={12}
+        ></GoogleMap>
+      </LoadScript>
+    );
+  }
+}
