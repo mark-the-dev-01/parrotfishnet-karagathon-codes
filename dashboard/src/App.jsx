8cd879f428a2769c6b7aa8f2e7f928c95e67c959
Mark-the-Dev <mark.the.dev.01@gmail.com>
2020-08-12 01:07:07 -0500

    using material-dashboard-react

--- dashboard/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import LeafletMap from "./components/LeafletMap/LeafletMap";
-
-export default class App extends Component {
-  render() {
-    return <LeafletMap />;
-  }
-}
---
+++ dashboard/src/views/LeafletMap/LeafletMap.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./LeafletMap.css";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function LeafletMap() {
+  const mapState = {
+    lat: 8.545379,
+    lng: 124.566067,
+    zoom: 11,
+  };
+  const position = [mapState.lat, mapState.lng];
+
+  return (
+    <Map
+      center={position}
+      zoom={mapState.zoom}
+      className="Leaflet-map-container"
+    >
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>a pretty customizable CSS3 popup.</Popup>
+      </Marker>
+    </Map>
+  );
+}
