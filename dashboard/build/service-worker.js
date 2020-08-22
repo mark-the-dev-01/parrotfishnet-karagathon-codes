8a5cd23fb3ae6bac7654c39841c35c58c6e1b338
Mark-the-Dev <mark.the.dev.01@gmail.com>
2020-08-22 17:32:56 -0500

    with build deployed aug22

--- dashboard/build/service-worker.js
+++ dashboard/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d040a36facc7bc57ccc9c20571d7b7aa.js"
+  "/precache-manifest.26809e395a4fa21c4473805774063e31.js"
 );
 
 self.addEventListener('message', (event) => {
